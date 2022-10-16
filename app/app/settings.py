ae9985cc4b664f3208b31bd2d5533611d29ebc63
abdulwahab alshehri <abdul.2020alsh@gmail.com>
2022-10-16 18:01:08 +0300

    fix db connection env var and testing

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +77,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
         'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSOWRD': os.environ.get('DB_PASS'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
